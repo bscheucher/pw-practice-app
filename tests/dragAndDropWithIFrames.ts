aaf7bb03610db18b1ab366c6650d43227cd1a843
bscheucher <bernhard.scheucher@gmail.com>
2026-01-02 18:18:00 +0100

    drag & drop iframe

--- tests/dragAndDropWithIFrames.ts
+++
@@ -1,7 +0,0 @@
-import test, { chromium, expect } from '@playwright/test'
-
-test('drag and drop with iframe', async ({page}) => {
-    await page.goto('https://www.globalsqa.com/demo-site/draganddrop/')
-
-    await page.locator('li', {hasText: "High Tatras"}).click()
-})
